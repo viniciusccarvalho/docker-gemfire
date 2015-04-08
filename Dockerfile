21fdb5948a48326aa4cc2e9426bf2200890f9eea
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2015-04-07 21:48:08 -0400

    first commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM java:8
+VOLUME /tmp
+EXPOSE 8080 1099 40404
+ADD config/cache.properties config/cache.properties
+ADD build/libs/gemfire-boot-server-8.1.0.jar gemfire-server.jar
+ENTRYPOINT ["java","-jar","/gemfire-server.jar"]
---
+++ src/main/java/org/springframework/gemfire/server/Application.java
@@ -0,0 +1,18 @@
+package org.springframework.gemfire.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created by vcarvalho on 4/7/15.
+ */
+@SpringBootApplication
+@ImportResource("/cache-context.xml")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.run(args);
+    }
+}
